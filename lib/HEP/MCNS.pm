8b25cedc1628aa2ff860a9102a38c1263fcd0431
Philipp Hamer <philipp@gnuzifer.de>
2015-05-12 09:23:36 +0200

    Build results of feb2919 (on master)

--- lib/HEP/MCNS.pm
+++ lib/HEP/MCNS.pm
@@ -635,12 +635,9 @@ HEP::MCNS - converts HEP MC numbers into particle names
 
 version 0.01
 
-=head1 SYNOPSIS
-	use HEP::MCNS 'particle_name';
-	my $electron_name = particle_name( 11 );
-	my $bzero_name = particle_name( 511 );
+=head1 FUNCTIONS
 
-=func particle_name()
+=head2 particle_name()
 
 takes the Monte Carlo number as input and prints the particle name 
 
